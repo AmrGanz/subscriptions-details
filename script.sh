ec8aef2379a4c819c345946da9fd3633c1736bab
Amr Elganzory <43642001+AmrGanz@users.noreply.github.com>
2020-04-20 13:56:58 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
-# Follow instructions here https://access.redhat.com/management/api to grab your token
+offline_token=''
 
-offline_token='PASTE HERE'
-
-# add contract id here
 contractid=
 
 function jsonValue() {
@@ -13,22 +10,21 @@ awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d 
 
 token=`curl -s https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token -d grant_type=refresh_token -d client_id=rhsm-api -d refresh_token=$offline_token | jsonValue access_token`
 
-subid=`curl -s -H "Authorization: Bearer $token" -X GET "https://api.access.redhat.com/management/v1/subscriptions" -H "accept: application/json" | jq -r --arg contractid "$contractid" '.body[] | select( .contractNumber == $contractid)' | jq -r .subscriptionNumber`
 
-subname=`curl -s -H "Authorization: Bearer $token" -X GET "https://api.access.redhat.com/management/v1/subscriptions" -H "accept: application/json" | jq -r --arg contractid "$contractid" '.body[] | select( .contractNumber == $contractid)' | jq -r .subscriptionName`
+curl -s -H "Authorization: Bearer $token" -X GET "https://api.access.redhat.com/management/v1/subscriptions" -H "accept: application/json" | jq -r --arg contractid "$contractid" '.body[] | select( .contractNumber == $contractid)' | jq -r .subscriptionNumber > /tmp/subid
 
-for i in $subid
+
+for subid in `cat /tmp/subid`
 do
 	echo "===================================================="
 	echo "subscription ID: $subid"
-        echo "Subscription Name: $subname"
+	subname=`curl -s -H "Authorization: Bearer $token" -X GET "https://api.access.redhat.com/management/v1/subscriptions" -H "accept: application/json" | jq -r --arg subid "$subid" '.body[] | select( .subscriptionNumber == $subid)' | jq -r .subscriptionName`
+	echo "Subscription Name: $subname"
 	systems=`curl -s -H "Authorization: Bearer $token" -X GET "https://api.access.redhat.com/management/v1/subscriptions/$subid/systems" -H "accept: application/json"  | jq -r '.body[].systemName'`
+	echo "Systems:"
 	for x in $systems
-	do	
-		echo "System name: $systems"
-		systemtype=`curl -s -H "Authorization: Bearer $token" -X GET "https://api.access.redhat.com/management/v1/subscriptions/$subid/systems" -H "accept: application/json"  | jq -r '.body[].type'`
-		echo "System Type: $systemtype"
+	do
+		echo $x
 	done
 	echo "===================================================="
 done
-
