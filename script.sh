49477181d105c2c5e5d93140203b94273885515c
Amr Elganzory <43642001+AmrGanz@users.noreply.github.com>
2020-04-20 16:50:52 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@ offline_token=''
 
 contractid=
 
+
 function jsonValue() {
 KEY=$1
 num=$2
@@ -10,9 +11,7 @@ awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d 
 
 token=`curl -s https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token -d grant_type=refresh_token -d client_id=rhsm-api -d refresh_token=$offline_token | jsonValue access_token`
 
-
 curl -s -H "Authorization: Bearer $token" -X GET "https://api.access.redhat.com/management/v1/subscriptions" -H "accept: application/json" | jq -r --arg contractid "$contractid" '.body[] | select( .contractNumber == $contractid)' | jq -r .subscriptionNumber > /tmp/subid
-
 
 for subid in `cat /tmp/subid`
 do
@@ -28,3 +27,5 @@ do
 	done
 	echo "===================================================="
 done
+
+
